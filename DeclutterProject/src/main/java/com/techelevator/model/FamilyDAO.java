d4650090028e5d0beb42563a588ee45679071924
Irina Tin <irina.tin@me.com>
2020-06-22 19:29:18 -0400

    FamilyDAO created

---
+++ DeclutterProject/src/main/java/com/techelevator/model/FamilyDAO.java
@@ -0,0 +1,20 @@
+package com.techelevator.model;
+
+import java.util.List;
+
+public interface FamilyDAO {
+	
+	public List<Family> getAllFamilies();
+	
+	public boolean createFamily(String familyName);
+	
+	public String getFamilyNameById(long familyId);
+	
+	public long getFamilyIdByName(String familyName);
+
+	public boolean doesFamilyExist(String familyName);
+	
+	public List<UserInfo> getAllFamilyMembers (Long familyId);
+	
+
+}
