3f8dc5db61c883b54cde0ae3259b0bce7564678f
Irina Tin <irina.tin@me.com>
2020-06-30 20:50:33 -0400

    Added stuff in schema.sql

--- DeclutterProject/src/main/java/com/techelevator/database/schema.sql
+++ DeclutterProject/src/main/java/com/techelevator/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN;
 
 DROP TABLE if exists users;
+DROP TABLE if exists stuff;
 
 
 CREATE TABLE users (
@@ -10,5 +11,12 @@ CREATE TABLE users (
  salt varchar(256) NOT NULL
  
  );
+ 
+ CREATE TABLE stuff (
+ id serial primary key,
+ item varchar(255) NOT NULL
+ --add foregn keys for category id and family id
+ );
+ 
 
 COMMIT;
