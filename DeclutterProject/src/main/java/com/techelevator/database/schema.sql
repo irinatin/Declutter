2b3aba3ba8354d2803970ac0979c192b24e31002
Irina Tin <irina.tin@me.com>
2020-07-04 21:58:08 -0400

    data.sql file created

--- DeclutterProject/src/main/java/com/techelevator/database/schema.sql
+++ DeclutterProject/src/main/java/com/techelevator/database/schema.sql
@@ -1,25 +1,10 @@
 BEGIN;
 
-DROP TABLE if exists users;
-DROP TABLE if exists stuff;
-DROP TABLE if exists family;
-DROP TABLE if exists user_info;
+DROP TABLE if exists users CASCADE;
+DROP TABLE if exists stuff CASCADE;
+DROP TABLE if exists family CASCADE;
+DROP TABLE if exists user_info CASCADE;
 
-
-CREATE TABLE users (
- id serial primary key,
- username varchar(255) NOT NULL UNIQUE,
- password varchar(32) NOT NULL,
- salt varchar(256) NOT NULL
- 
- );
- 
- CREATE TABLE stuff (
- stuff_id serial primary key,
- item varchar(255) NOT NULL
- --add foregn keys for category id and family id
- );
- 
  CREATE TABLE family (
  family_id serial NOT NULL,
  family_name varchar(255) NOT NULL,
@@ -27,8 +12,17 @@ CREATE TABLE users (
  CONSTRAINT pk_family_id PRIMARY KEY(family_id)
  
  );
+
  
- CREATE TABLE user_info (
+CREATE TABLE users (
+ id serial PRIMARY KEY,
+ username varchar(255) NOT NULL UNIQUE,
+ password varchar(32) NOT NULL,
+ salt varchar(256) NOT NULL
+ 
+ );
+ 
+  CREATE TABLE user_info (
   user_info_id serial PRIMARY KEY,
   user_id int NOT NULL,
   first_name varchar(100) NOT NULL,
@@ -39,6 +33,19 @@ CREATE TABLE users (
   CONSTRAINT fk_family_id FOREIGN KEY (family_id) REFERENCES family (family_id)
 );
  
+ CREATE TABLE stuff (
+ stuff_id serial primary key,
+ item varchar(255) NOT NULL,
  
+CONSTRAINT pk_stuff_id PRIMARY KEY (stuff_id)
+ --add foregn keys for category id and family id
+ );
+ 
+
+ 
+
+ 
+--ALTER table stuff
+--ADD column 
 
 COMMIT;
